57140081d046a9fcff40a1eff5ee17751d01d657
vinod vetukuri <92874615+vinod4362@users.noreply.github.com>
2021-12-01 23:44:41 +0530

    Add files via upload

---
+++ SPRING/SPRING CORE/SPRING MVC/MVC1/src/main/java/hello/HelloController.java
@@ -0,0 +1,21 @@
+package hello;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloController {
+	@RequestMapping("/helloWorld")
+		public String hello() {
+			return "helloWorld";
+		}
+	
+}
---
+++ SPRING/SPRING CORE/SPRING MVC/Mvc3/src/main/java/mvc3/UserModel.java
@@ -0,0 +1,10 @@
+package mvc3;
+
+
+public class UserModel {
+
+	String userName = "Himanshu";
+	String password = "Patil";
+	String email = "himanshup2098@gmail.com";
+	
+}
