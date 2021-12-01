32b75fad0374a055fbaacf29d10b32b8f22a8ee7
vinod vetukuri <92874615+vinod4362@users.noreply.github.com>
2021-12-01 23:48:10 +0530

    Add files via upload

---
+++ SPRING/SPRING CORE/SPRING REST/SpringRest4/src/main/java/com/example/RestAssignment/RestAssignmentApplication.java
@@ -0,0 +1,16 @@
+package com.example.RestAssignment;
+
+ 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestAssignmentApplication {
+
+	public static void main(String[] args) {
+		
+	 SpringApplication.run(RestAssignmentApplication.class, args);;
+		
+		}
+	}
+ 
