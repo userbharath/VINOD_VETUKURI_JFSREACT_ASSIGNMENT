ad828e02e4fe3d4172f7906696cbd6bb336013eb
vinod vetukuri <92874615+vinod4362@users.noreply.github.com>
2021-12-01 23:38:14 +0530

    Delete SPRING directory

--- SPRING/SPRING CORE/SPRING REST/SpringRest8/src/main/java/com/Question_8/QNo_8/services/ProductMongoRepo.java
+++
@@ -1,9 +0,0 @@
-package com.Question_8.QNo_8.services;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.Question_8.QNo_8.Product;
-
-public interface ProductMongoRepo extends MongoRepository<Product, Integer> {
-
-}
--- SPRING/SPRING CORE/SPRING REST/SpringRest9/target/classes/data.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO 
-	TBL_CUSTOMERS (first_name, last_name, email) 
-VALUES
-  	('okay', 'computing', 'okaycomputing@gmail.com'),
-  	('spring', 'h2db', 'spring.h2db@gmail.com'),
-  	('springboot', 'in-memory-db', 'springboot.inmemory@gmail.com');
-  	
